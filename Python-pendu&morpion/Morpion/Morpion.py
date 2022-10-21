4b69b0542f0c586848c170ce13081139068d0c36
mohammed yassine dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2022-10-21 16:28:48 +0200

    [Premier pas Morpion]

--- Python-pendu&morpion/Morpion/Morpion.py
+++ Python-pendu&morpion/Morpion/Morpion.py
@@ -2,7 +2,34 @@
 #                      La Plateforme 2022
 
 
+jouerOuScore=input("Bonjour souhaites tu jouer ou voir les scores ? ")
+#fonction pour affiché la matrice du jeu 
+def affichageMatrice(M):
+    for i in range (len(M)): 
+        for j in range (len(M)):
+            print(M[i][j], end= " ")
+        print("")    
 
-scorejoueur1=0
-scoreJoueur2=0
-jouerOuScore=input("Bonjourn souhaites tu jouer ou voir les scores ? ")
+
+#condition pour Joueur ou affiche le score 
+if jouerOuScore=="Jouer" or jouerOuScore=="jouer":
+    print("Jouer")
+    
+    #Saisie des noms des joueurs 
+    nomJoueur1=input("Username 1 Croix : ")
+    nomJoueur2=input("Username 2 Rond  : ")
+    
+    #initialisation de la matrice et le nombre de lignes et de colonnes 
+    M=[['-', '-', '-'],['-', '-', '-'],['-', '-', '-']]
+    
+    #Premier affichage  
+    affichageMatrice(M)   
+    
+    #Début du jeu 
+    
+    jeu1=print(nomJoueur1, " joue (ligne puis colonne) : ")
+
+    
+
+elif jouerOuScore=="Score" or jouerOuScore=="score":
+    print("score")
